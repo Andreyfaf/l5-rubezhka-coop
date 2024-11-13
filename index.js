0b2115741132c825b67d9ba0ef99d9e811bd7095
Andrey <andrey.fafonov2003@mail.ru>
2024-11-13 10:03:49 +0300

    rubezhka

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import PostalValidator from './src/PostalCodeSchema';
+import PostalValidator from './src/PostalCodeSchema.js';
+import IpValidator from './src/IpAddressSchema.js';
 
 class Validator {
   postalCode() {
     return new PostalValidator();
   }
 
-  // ipAddress() {
-  //   return new IpValidator();
-  // }
+  ipAddress() {
+    return new IpValidator();
+  }
 }
 
 export default Validator;
